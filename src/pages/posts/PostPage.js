1a4165e01b295dc8f4fdb981a09ae42ea33705a8
DOdrums <dirkornee@hotmail.com>
2022-12-13 14:09:44 +0000

    Add CommentCreateForm

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from "react";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router-dom";
@@ -12,6 +14,9 @@ import Post from "./Post";
 function PostPage() {
   const { id } = useParams();
   const [post, setPost] = useState({ results: [] })
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
 
   useEffect(() => {
     const handleMount = async () => {
@@ -37,7 +42,17 @@ function PostPage() {
         <p>Post component</p>
         <Post {...post.results[0]} setPosts={setPost} postPage />
         <Container className={appStyles.Content}>
-          Comments
+        {currentUser ? (
+          <CommentCreateForm
+            profile_id={currentUser.profile_id}
+            profileImage={profile_image}
+            post={id}
+            setPost={setPost}
+            setComments={setComments}
+          />
+          ) : comments.results.length ? (
+            "Comments"
+          ) : null}
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
